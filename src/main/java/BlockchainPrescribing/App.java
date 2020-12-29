0be883cc084c8f7828c8276fc68651305b97825d
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2020-12-29 20:26:36 +0000

    Created Java maven project to run application from

---
+++ src/main/java/BlockchainPrescribing/App.java
@@ -0,0 +1,13 @@
+package BlockchainPrescribing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
