8ffb1b5fd59df3a38f6f6473dc903e5fba4b20c0
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-01-22 16:52:10 +0000

    Created manual script to set enviromental variables and set the java version

---
+++ config-env.sh
@@ -0,0 +1,4 @@
+export PATH=/home/dan/Docs/fabric-samples/bin:$PATH
+export FABRIC_CFG_PATH=/home/dan/Docs/fabric-samples/config/
+export PATH=$PATH:$FABRIC_CFG_PATH 
+sudo update-alternatives --config java
