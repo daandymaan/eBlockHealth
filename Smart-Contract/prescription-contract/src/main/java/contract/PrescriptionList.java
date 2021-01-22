011197a6946e3230241f19315e4d4dcb9606af38
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-01-22 21:08:01 +0000

    Created PrescriptionList class to allow retrieval of prescriptions

---
+++ Smart-Contract/prescription-contract/src/main/java/contract/PrescriptionList.java
@@ -0,0 +1,28 @@
+package contract;
+
+import org.hyperledger.fabric.contract.Context;
+
+import contract.ledgerapi.StateList;
+
+public class PrescriptionList {
+    
+    private StateList stateList;
+
+    public PrescriptionList(Context ctx){
+        this.stateList = StateList.getStateList(ctx, PrescriptionList.class.getSimpleName(), Prescription::deserialize);
+    }
+    
+    public PrescriptionList addPrescription(Prescription prescription){
+        stateList.addState(prescription);
+        return this;
+    }
+
+    public Prescription getPrescription(String key){
+        return(Prescription) this.stateList.getState(key);
+    }
+
+    public PrescriptionList updatePrescription(Prescription prescription){
+        this.stateList.updateState(prescription);
+        return this;
+    }
+}
