319445cad32e604a323407adc73c9bee174b0f97
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-01-23 16:11:48 +0000

    Edited config-env script to allow PEER enviromental variables to be changed

--- config-env.sh
+++ config-env.sh
@@ -1,3 +1,21 @@
+
+: ${CHANNEL_NAME:="mychannel"}
+: ${DELAY:="3"}
+: ${MAX_RETRY:="5"}
+: ${VERBOSE:="false"}
+
+
+#CD into test network directory 
+cd /home/dan/Docs/fabric-samples/test-network/
+OVERRIDE_ORG="2"
+. ./scripts/envVar.sh
+
+#Setting peer parameters to use when deploying code
+parsePeerConnectionParameters 1 2
+export PEER_PARMS="${PEER_CONN_PARMS##*( )}"
+
+
+#Setting enviromental variables
 export PATH=/home/dan/Docs/fabric-samples/bin:$PATH
 export FABRIC_CFG_PATH=/home/dan/Docs/fabric-samples/config/
 export PATH=$PATH:$FABRIC_CFG_PATH 
