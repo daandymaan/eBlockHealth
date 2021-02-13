debeebcba6ed83dc59767d1e85c6b4c53968f3c0
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-02-13 17:15:50 +0000

    Modification of the config-env script. This modification initializes the enviromental variables correctly.

--- config-env.sh
+++ config-env.sh
@@ -1,54 +1,134 @@
 #!/bin/bash
 #
-# SPDX-License-Identifier: Apache-2.0
-shopt -s extglob
+C_RESET='\033[0m'
+C_RED='\033[0;31m'
+C_GREEN='\033[0;32m'
+C_BLUE='\033[0;34m'
+C_YELLOW='\033[1;33m'
 
-function _exit(){
-    printf "Exiting:%s\n" "$1"
-    exit -1
+PATH2NETWORK=/home/dan/Docs/fabric-samples/test-network
+
+# println echos string
+function println() {
+  echo -e "$1"
 }
 
-re='^[1-2]+$'
-if ! [[ $1 =~ $re ]] ; then
-   echo "error: Not a number" >&2; exit 2
-fi
+# errorln echos i red color
+function errorln() {
+  println "${C_RED}${1}${C_RESET}"
+}
 
-: ${CHANNEL_NAME:="mychannel"}
-: ${DELAY:="3"}
-: ${MAX_RETRY:="5"}
-: ${VERBOSE:="false"}
+# successln echos in green color
+function successln() {
+  println "${C_GREEN}${1}${C_RESET}"
+}
 
+# infoln echos in blue color
+function infoln() {
+  println "${C_BLUE}${1}${C_RESET}"
+}
 
-DIR=${PWD}
+# warnln echos in yellow color
+function warnln() {
+  println "${C_YELLOW}${1}${C_RESET}"
+}
 
-#CD into test network directory 
-cd /home/dan/Docs/fabric-samples/test-network/
-env | sort > /tmp/env.orig
-
-if [ $1 == 1 ]
-then 
-    #Admin 2 
-    OVERRIDE_ORG="1"
-else 
-    #Admin 1
-    OVERRIDE_ORG="2"
-fi 
-. ./scripts/envVar.sh
-
-#Setting peer parameters to use when deploying code
-parsePeerConnectionParameters 1 2
-export PEER_PARMS="${PEER_CONN_PARMS##*( )}"
+# fatalln echos in red color and exits with fail status
+function fatalln() {
+  errorln "$1"
+  exit 1
+}
 
 
-#Setting enviromental variables
+infoln "Setting enviromental variables"
 export PATH=/home/dan/Docs/fabric-samples/bin:$PATH
 export FABRIC_CFG_PATH=/home/dan/Docs/fabric-samples/config/
 export PATH=$PATH:$FABRIC_CFG_PATH 
 
+infoln "Setting peer variables"
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=$PATH2NETWORK/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=$PATH2NETWORK/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export PEER0_ORG2_CA=$PATH2NETWORK/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export PEER0_ORG3_CA=$PATH2NETWORK/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
 
-env | sort | comm -1 -3 /tmp/env.orig - | sed -E 's/(.*)=(.*)/export \1="\2"/'
-rm /tmp/env.orig
+setGlobals() {
+infoln "Setting globals"
+  local USING_ORG=""
+  if [ -z "$OVERRIDE_ORG" ]; then
+    USING_ORG=$1
+  else
+    USING_ORG="${OVERRIDE_ORG}"
+  fi
+  infoln "Using organization ${USING_ORG}"
+  if [ $USING_ORG -eq 1 ]; then
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=$PATH2NETWORK/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+  elif [ $USING_ORG -eq 2 ]; then
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=$PATH2NETWORK/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
 
-#sudo update-alternatives --config java
+  elif [ $USING_ORG -eq 3 ]; then
+    export CORE_PEER_LOCALMSPID="Org3MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG3_CA
+    export CORE_PEER_MSPCONFIGPATH=$PATH2NETWORK/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:11051
+  else
+    errorln "ORG Unknown"
+  fi
 
-cd ${DIR}
+  if [ "$VERBOSE" == "true" ]; then
+    env | grep CORE
+  fi
+}
+
+setGlobalsCLI() {
+infoln "Set environment variables for use in the CLI container"
+  setGlobals $1
+
+  local USING_ORG=""
+  if [ -z "$OVERRIDE_ORG" ]; then
+    USING_ORG=$1
+  else
+    USING_ORG="${OVERRIDE_ORG}"
+  fi
+  if [ $USING_ORG -eq 1 ]; then
+    export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+  elif [ $USING_ORG -eq 2 ]; then
+    export CORE_PEER_ADDRESS=peer0.org2.example.com:9051
+  elif [ $USING_ORG -eq 3 ]; then
+    export CORE_PEER_ADDRESS=peer0.org3.example.com:11051
+  else
+    errorln "ORG Unknown"
+  fi
+}
+
+parsePeerConnectionParameters() {
+infoln "Helper function that sets the peer connection parameters for a chaincode operation"
+  PEER_CONN_PARMS=""
+  PEERS=""
+  while [ "$#" -gt 0 ]; do
+    setGlobals $1
+    PEER="peer0.org$1"
+    ## Set peer addresses
+    PEERS="$PEERS $PEER"
+    PEER_CONN_PARMS="$PEER_CONN_PARMS --peerAddresses $CORE_PEER_ADDRESS"
+    ## Set path to TLS certificate
+    TLSINFO=$(eval echo "--tlsRootCertFiles \$PEER0_ORG$1_CA")
+    PEER_CONN_PARMS="$PEER_CONN_PARMS $TLSINFO"
+    # shift by one to get to the next organization
+    shift
+  done
+  # remove leading space for output
+  PEERS="$(echo -e "$PEERS" | sed -e 's/^[[:space:]]*//')"
+}
+
+verifyResult() {
+  if [ $1 -ne 0 ]; then
+    fatalln "$2"
+  fi
+}
