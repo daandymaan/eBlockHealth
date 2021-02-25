5f5fca26a50a01b2aac30d60ed3cba21fc4c8a3c
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-02-25 17:20:48 +0000

    Slight changes to authentication

--- Application/app/src/main/java/application/Authentication.java
+++ Application/app/src/main/java/application/Authentication.java
@@ -12,24 +12,30 @@ import org.hyperledger.fabric.gateway.Wallets;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+@javax.ws.rs.Path("/authentication")
 public class Authentication {
 
+    @javax.ws.rs.Path("/validateUser")
     public JsonObject authenticateUser(JsonObject user) throws Exception {
-        //Getting the wallet
-        Path walletpath = Paths.get("../../wallets");
-        Wallet wallet = Wallets.newFileSystemWallet(walletpath);
-
-        //Detecting if the user is present in the wallet
-        if(wallet.get(user.get("PPSN").getAsString()) == null){
-            System.out.println(user.get("PPSN").getAsString() + " does not exists");
-        } else {
-            System.out.println(user.get("PPSN").getAsString() + " found!");
-            String passphrase = readPK(user.get("PPSN").getAsString());
+        if(userExists(user).equals("success")){
+            System.out.println(user.get("identifier").getAsString() + " found!");
+            String passphrase = readPK(user.get("identifier").getAsString());
             if(verifyPK(user, passphrase) == 0){
                 user = addPublicKey(user);
             }
         }
         return user;
+    }
+
+    @javax.ws.rs.Path("/verifyUser")
+    public String userExists(JsonObject user) throws IOException {
+        Path walletpath = Paths.get("../../wallets");
+        Wallet wallet = Wallets.newFileSystemWallet(walletpath);
+        if(wallet.get(user.get("identifier").getAsString()) != null){
+            return "success";
+        } else {
+            return "failure";
+        }
     }
 
     public String readPK(String ppsn){     
--- Application/app/src/main/java/application/Connection.java
+++ Application/app/src/main/java/application/Connection.java
@@ -19,17 +19,23 @@ public class Connection {
 		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
 	}
     
-    public static Contract connect(JsonObject user) throws Exception{
+    public static Network connect(JsonObject user) throws Exception{
         Path walletpath = Paths.get("../../wallets");
         Wallet wallet = Wallets.newFileSystemWallet(walletpath);
         Path networkConfigPath = Paths.get("../../Orgs/Org1/gateway/connection-org1.yaml");
         Gateway.Builder builder = Gateway.createBuilder();
-        builder.identity(wallet, user.get("PPSN").getAsString()).networkConfig(networkConfigPath).discovery(true);
+        builder.identity(wallet, user.get("identifier").getAsString()).networkConfig(networkConfigPath).discovery(true);
         Gateway gateway = builder.connect();
         System.out.println("Gateway created");
         Network network = gateway.getNetwork("mychannel");
         System.out.println("Network created");
-        Contract contract = network.getContract("pc");
+        return network;
+
+    }
+
+    public static Contract getContract(JsonObject user, String contractName) throws Exception {
+        Network network = connect(user);
+        Contract contract = network.getContract(contractName);
         System.out.println("Contract found");
         return contract;
     }
