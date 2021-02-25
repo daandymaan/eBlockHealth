4eb7b53cf0ffbec13e1915b80d6c263447aa932e
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-02-25 17:21:26 +0000

    Created a gatway for api to access ledger methods

--- Application/app/src/main/java/application/PrescriptionRequests.java
+++ Application/app/src/main/java/application/PrescriptionRequests.java
@@ -12,39 +12,48 @@ public class PrescriptionRequests {
     // PrescriptionContext ctx, String date, String issuer, String product, String
     // productID, String productPackage, String quantity, String doseStrength,
     // String doseType, String doseQuantity, String instruction, String comment
-    public static void createPrescription(Contract contract, JsonObject data, JsonObject user) {
+    public static void createPrescription(JsonObject data, JsonObject user) {
         System.out.println("Creating prescription");
+
         try {
-            byte[] result = contract.submitTransaction("issue", 
-            data.get("date").getAsString(), 
-            user.get("cert").getAsString(),
-            data.get("product").getAsString(), 
-            data.get("productID").getAsString(),
-            data.get("productPackage").getAsString(), 
-            data.get("quantity").getAsString(),
-            data.get("doseStrength").getAsString(),
-            data.get("doseType").getAsString(),
-            data.get("doseQuantity").getAsString(),
-            data.get("instruction").getAsString(),
-            data.get("comment").getAsString());
+            Contract contract = Connection.getContract(user, "pc");
+            byte[] result = contract.submitTransaction("issue", data.get("date").getAsString(),
+                    user.get("cert").getAsString(), data.get("product").getAsString(),
+                    data.get("productID").getAsString(), data.get("productPackage").getAsString(),
+                    data.get("quantity").getAsString(), data.get("doseStrength").getAsString(),
+                    data.get("doseType").getAsString(), data.get("doseQuantity").getAsString(),
+                    data.get("instruction").getAsString(), data.get("comment").getAsString());
             System.out.println(new String(result));
         } catch (ContractException | TimeoutException | InterruptedException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static void getAllPrescriptions(Contract contract){
-        System.out.println("Get all prescriptions");
-        byte[] result;
-        try {
-            result = contract.evaluateTransaction("getAllPrescriptions");
-            System.out.println(new String(result));
-        } catch (ContractException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void transferPrescription(){
+    public static void getAllPrescriptions(JsonObject user) {
         System.out.println("Get all prescriptions");
+        byte[] result;
+        try {
+            Contract contract = Connection.getContract(user, "pc");
+            result = contract.evaluateTransaction("getAllPrescriptions");
+            System.out.println(new String(result));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String transferPrescription(JsonObject user, String PID, String newOwner) {
+        System.out.println("Transfer prescription");
+        byte[] result;
+        try {
+            Contract contract = Connection.getContract(user, "pc");
+            result = contract.submitTransaction("transferPrescription", PID, newOwner);
+            return new String(result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Could not transfer prescription";
+        }
     }
 }
---
+++ Application/app/src/main/java/application/PrescriptionRequestsGateway.java
@@ -0,0 +1,32 @@
+package application;
+
+import javax.ws.rs.Path;
+
+@Path("/prescriptionRequests")
+public class PrescriptionRequestsGateway {
+    
+    public String getAllPrescriptions(){
+
+    }
+
+    public String getPrescriptionsForUser(){
+
+    }
+
+    public String updatePrescriptionStatus(){
+
+    }
+
+    public String updatePrescriptionOwner(){
+
+    }
+
+    public String updatePrescription(){
+
+    }
+
+    public String createPrescription(){
+        
+    }
+
+}
