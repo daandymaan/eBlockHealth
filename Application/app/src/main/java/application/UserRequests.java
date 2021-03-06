2001ddef00d864d7029480693192eccbec114acd
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-03-06 15:21:07 +0000

    #4 #12 Created REST api calls for user Only some of these will be used

--- Application/app/src/main/java/application/UserRequests.java
+++ Application/app/src/main/java/application/UserRequests.java
@@ -6,14 +6,17 @@ import java.util.List;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import org.hyperledger.fabric.gateway.Contract;
 import org.json.JSONObject;
 
 public class UserRequests {
 
-    public String getUsers(JsonObject user) {
+    public static String getUsers(JsonObject user) {
         System.out.println("Get all users");
         byte result[];
         try {
@@ -22,59 +25,62 @@ public class UserRequests {
             return new String(result);
         } catch (Exception e) {
             e.printStackTrace();
-            return "No user information found";
+            JsonObject errorResponse = new JsonObject();
+            errorResponse.addProperty("msg", "Users could not be found");
+            return errorResponse.toString();
         }
     }
 
-    public String getUserByIdentifier(JsonObject user, String identifier) {
+    public static String getUserByIdentifier(JsonObject user, String identifier) {
         System.out.println("Get user by identifier");
-        ObjectMapper mapper = new ObjectMapper();
-        String results = getUsers(user);
-        try {
-            List<User> users = Arrays.asList(mapper.readValue(results, User[].class));
-            for(User current : users){
-                if(current.getIdentifier().equals(identifier)){
-                    return new String(User.serialize(current));
-                }
+        JsonObject userSelected = new JsonObject();
+        JsonArray JSONResults = JsonParser.parseString(UserRequests.getUsers(user)).getAsJsonArray();
+
+        for (JsonElement jsonElement : JSONResults) {
+            if(jsonElement.getAsJsonObject().get("identifier").getAsString().equals(identifier)){
+                userSelected = jsonElement.getAsJsonObject();
             }
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
         }
-        return "Could not find user";
+
+        if(!userSelected.entrySet().isEmpty()){
+            return userSelected.toString();
+        } else {
+            JsonObject errorResponse = new JsonObject();
+            errorResponse.addProperty("msg", "User could not be found");
+            return errorResponse.toString();
+        }
+
     }
 
-    public String updateUser(JsonObject user) {
+    public static String updateUser(JsonObject user, String identifier, String title, String firstname, 
+    String surname, String address, String dob, String gender, String email, String status, String cert) {
         System.out.println("Update user");
         byte result[];
         try {
             Contract contract = Connection.getContract(user, "infocontract");
-            result = contract.submitTransaction("updateUser", user.get("identifier").getAsString(),
-                    user.get("title").getAsString(), user.get("firstname").getAsString(),
-                    user.get("surname").getAsString(), user.get("address").getAsString(), user.get("dob").getAsString(),
-                    user.get("gender").getAsString(), user.get("email").getAsString(), user.get("status").getAsString(),
-                    user.get("cert").getAsString());
+            result = contract.submitTransaction("updateUser", identifier, title, firstname, surname, address, dob, gender, email, status, cert);
             return new String(result);
         } catch (Exception e) {
             e.printStackTrace();
-            return "Could not update user";
+            JsonObject errorResponse = new JsonObject();
+            errorResponse.addProperty("msg", "User could not be updated");
+            return errorResponse.toString();
         }
 
     }
 
-    public String createUser(JsonObject user) {
+    public static String createUser(JsonObject user, String identifier, String title, String firstname, 
+    String surname, String address, String dob, String gender, String email, String status, String cert) {
         System.out.println("Create user");
         byte result[];
         try {
             Contract contract = Connection.getContract(user, "infocontract");
-            result = contract.submitTransaction("createUser", user.get("identifier").getAsString(),
-            user.get("title").getAsString(), user.get("firstname").getAsString(),
-            user.get("surname").getAsString(), user.get("address").getAsString(), user.get("dob").getAsString(),
-            user.get("gender").getAsString(), user.get("email").getAsString(), user.get("status").getAsString(),
-            user.get("cert").getAsString());
+            result = contract.submitTransaction("createUser", identifier, title, firstname, surname, address, dob, gender, email, status, cert);
             return new String(result);
         } catch (Exception e) {
-            e.printStackTrace();
-            return "Could not create user";
+            JsonObject errorResponse = new JsonObject();
+            errorResponse.addProperty("msg", "User could not be created");
+            return errorResponse.toString();
         }
     }
 }
--- Application/app/src/main/java/application/UserRequestsGateway.java
+++ Application/app/src/main/java/application/UserRequestsGateway.java
@@ -1,24 +1,76 @@
 package application;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @Path("/userRequestsGateway")
 public class UserRequestsGateway {
     
+    @POST
     @Path("/getAllUsers")
-    public String getAllUsers(){
-
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllUsers(String user){
+        JsonObject userInfo = (JsonObject) JsonParser.parseString(user).getAsJsonObject();
+        return UserRequests.getUsers(userInfo);
     }
 
-    public String getUser(){
-
+    @POST
+    @Path("/getUser")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getUser(String request){
+        JsonObject requestJson = (JsonObject) JsonParser.parseString(request).getAsJsonObject();
+        JsonObject userInfo = requestJson.get("user").getAsJsonObject();
+        String identifier = requestJson.get("identifier").getAsString();
+        return UserRequests.getUserByIdentifier(userInfo, identifier);
     }
 
-    public String updateUserDetails(){
-
+    @POST
+    @Path("/updateUserDetails")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateUserDetails(String request){
+        JsonObject requestJson = (JsonObject) JsonParser.parseString(request).getAsJsonObject();
+        JsonObject userInfo = requestJson.get("user").getAsJsonObject();
+        JsonObject updatedUser = requestJson.get("updatedUser").getAsJsonObject();
+        String identifier = updatedUser.get("identifier").getAsString();
+        String title = updatedUser.get("title").getAsString();
+        String firstname = updatedUser.get("firstname").getAsString();
+        String surname = updatedUser.get("surname").getAsString();
+        String address = updatedUser.get("address").getAsString();
+        String dob = updatedUser.get("dOB").getAsString();
+        String gender = updatedUser.get("gender").getAsString();
+        String email  = updatedUser.get("email").getAsString();
+        String status = updatedUser.get("status").getAsString();
+        String cert = updatedUser.get("cert").getAsString();
+        return UserRequests.updateUser(userInfo, identifier, title, firstname, surname, address, dob, gender, email, status, cert);
     }
 
-    public String createUser(){
-
+    @POST
+    @Path("/createUser")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String createUser(String request){
+        JsonObject requestJson = (JsonObject) JsonParser.parseString(request).getAsJsonObject();
+        JsonObject userInfo = requestJson.get("user").getAsJsonObject();
+        JsonObject newUser = requestJson.get("updatedUser").getAsJsonObject();
+        String identifier = newUser.get("identifier").getAsString();
+        String title = newUser.get("title").getAsString();
+        String firstname = newUser.get("firstname").getAsString();
+        String surname = newUser.get("surname").getAsString();
+        String address = newUser.get("address").getAsString();
+        String dob = newUser.get("dOB").getAsString();
+        String gender = newUser.get("gender").getAsString();
+        String email  = newUser.get("email").getAsString();
+        String status = newUser.get("status").getAsString();
+        String cert = newUser.get("cert").getAsString();
+        return UserRequests.createUser(userInfo, identifier, title, firstname, surname, address, dob, gender, email, status, cert);
     }
 }
