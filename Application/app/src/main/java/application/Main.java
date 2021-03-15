2b98a18d3d83f394fa078f4814b7bd0ad98d5b39
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-03-15 19:58:23 +0000

    Updated and tested new functionality

--- Application/app/src/main/java/application/Main.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class Main extends Application{
-    
-}
--- Application/app/src/main/webapp/utils/InfoCard.js
+++ Application/app/src/main/webapp/utils/InfoCard.js
@@ -1,16 +1,18 @@
 import Server from "./Server.js";
 
 new ClipboardJS('.btn');
-
-Server.getDetailsForUser(sessionStorage.getItem("ID"), function(value){
+var identifier = {"identifier" : sessionStorage.getItem("ID")};
+var request = {"user" : identifier, "identifier" : sessionStorage.getItem("ID")}
+Server.getDetailsForUser(request, function(value){
+    console.log("value ", value);
     var ppsn = document.getElementById("PPSNNO");
-    ppsn.innerHTML = value.PPSN;
+    ppsn.innerHTML = value.identifier;
     var fullname = document.getElementById("NAME");
     fullname.innerHTML = value.firstname + " " + value.surname;
     var dob = document.getElementById("DOB");
-    dob.innerHTML = value.DOB
+    dob.innerHTML = value.dob
     var status = document.getElementById("STATUS");
     status.innerHTML = value.status;
     var cert = document.getElementById("cert");
-    cert.value = value.address;
+    cert.value = value.cert;
 })
