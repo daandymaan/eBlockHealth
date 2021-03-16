e496fa20eeff2afb6fb5f285d1e786354ecca3f9
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-03-16 21:34:29 +0000

    Altered prescription widget to display data from ledger #4 The owner and issuer certificates will display in input boxes to not overwhelm the user #12 #4

--- webapp/utils/PrescriptionWidget.js
+++ webapp/utils/PrescriptionWidget.js
@@ -1,18 +1,18 @@
 import Server from './Server.js'
-Server.getPrescriptionsForUser(sessionStorage.getItem("ID"), function(value){
+Server.getPrescriptionsForUser( function(value){
 
-    var tempdata = [{"PID": "0385736289", "date":"11/02/2021", "issuer": "GP", "owner": "Me", "product":"LEXAPRO", "productID":"G551",
+    var tempdata = [{"pID": "0385736289", "date":"11/02/2021", "issuer": "GP", "owner": "Me", "product":"LEXAPRO", "productID":"G551",
                     "productPackage":"", "quantity":"20", "doseStrength":"10MG",
                     "doseType":"TABS", "doseQuantity":"1perday"},
                 
-                    {"PID": "0020555562", "date":"11/02/2021", "issuer": "GP", "owner": "Me", "product":"CHEESESAUCE", "productID":"G5333",
+                    {"pID": "0020555562", "date":"11/02/2021", "issuer": "GP", "owner": "Me", "product":"CHEESESAUCE", "productID":"G5333",
                     "productPackage":"", "quantity":"20", "doseStrength":"10Slices",
                     "doseType":"TABS", "doseQuantity":"1perday"}];
-    var data = tempdata;
+    var data = value;
     
     var widgets = [];
 
-    tempdata.forEach(function(widgetData){
+    data.forEach(function(widgetData){
         widgets.push(widgetCreation(widgetData));
     })
 })
@@ -22,8 +22,8 @@ function widgetCreation(data){
 
     var widgetDiv = document.createElement("div");
     widgetDiv.className += "card";
-    var PIDH1 = document.createElement("h1");
-    PIDH1.className += "card-header card-title"
+    var pIDH1 = document.createElement("h1");
+    pIDH1.className += "card-header card-title"
     var DATEH2 = document.createElement("h2");
     DATEH2.className += "card-text";
     var PRODUCTH3 = document.createElement("h3");
@@ -49,7 +49,7 @@ function widgetCreation(data){
     sendButton.setAttribute("data-toggle", "modal");
     sendButton.setAttribute("data-target", "#modalView");
 
-    PIDH1.textContent = data.PID;
+    pIDH1.textContent = data.pID;
     DATEH2.textContent = data.date;
     PRODUCTH3.textContent = data.product;
     openButton.textContent = "View prescription details";
@@ -57,7 +57,7 @@ function widgetCreation(data){
 
     var dv = document.getElementById("prescription_widget_div");
     dv.appendChild(widgetDiv);
-    widgetDiv.appendChild(PIDH1);
+    widgetDiv.appendChild(pIDH1);
     widgetDiv.appendChild(DATEH2);
     widgetDiv.appendChild(PRODUCTH3);
     widgetDiv.appendChild(openButton);
@@ -73,16 +73,25 @@ function viewPrescriptionModal(data){
     }
     var contentString = "";
     var modalTitle = document.getElementById("modal-title");
-    modalTitle.innerHTML = data.PID;
+    modalTitle.innerHTML = data.pID;
     var keysCount = Object.keys(data).length;
     for(var i = 0; i < keysCount; i++){
         var currentKey = Object.keys(data)[i];
-        contentString += currentKey + " : " + data[currentKey];
-        contentString += "<br>"; 
+        if(currentKey != "owner" && currentKey != "issuer"){
+            contentString += currentKey.replace(/([a-z0-9])([A-Z])/g, '$1 $2') + " : " + data[currentKey];
+            contentString += "<br>"; 
+        }
     }
     var modalContent = document.getElementById("modal-body");
     modalContent.innerHTML = "";
     modalContent.innerHTML = contentString;
+
+    new ClipboardJS('#issuerbtn');
+    new ClipboardJS('#ownerbtn');
+    var ownerTextInput = document.getElementById("owner");
+    ownerTextInput.value = data.owner;
+    var issuerTextInput = document.getElementById("issuer");
+    issuerTextInput.value = data.issuer;
 }
 
 function transferPrescription(data){
@@ -97,11 +106,11 @@ function transferPrescription(data){
     submitButton.textContent = "Submit";
 
     submitButton.onclick = function(){
-        var transferData = {"PID" : undefined, "owner" : undefined, "recipient" : undefined};
+        var transferData = {"pID" : undefined, "owner" : undefined, "recipient" : undefined};
 
         if(inputBox.value != ""){
             console.log(inputBox.value);
-            transferData.PID = data.PID;
+            transferData.pID = data.pID;
             transferData.owner = data.owner;
             transferData.recipient = inputBox.value;
             Server.sendPrescriptionToUser(transferData, function(value){
@@ -114,7 +123,7 @@ function transferPrescription(data){
     modalFooter.appendChild(submitButton);
 
     var modalTitle = document.getElementById("modal-title");
-    modalTitle.innerHTML = data.PID;
+    modalTitle.innerHTML = data.pID;
 
     var modalContent = document.getElementById("modal-body");
     modalContent.innerHTML = "Please enter recipients address <br><br>";
