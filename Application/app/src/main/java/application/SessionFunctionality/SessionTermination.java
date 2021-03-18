fe376e8b129b56885aab68ee02197d33ac3875c2
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-03-18 22:59:11 +0000

    Added a proper implementation of logout so that it kills the session #12 #4

---
+++ Application/app/src/main/java/application/SessionFunctionality/SessionTermination.java
@@ -0,0 +1,41 @@
+package application.SessionFunctionality;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.JsonObject;
+
+import application.Logging;
+
+@WebServlet("/userRequestsGateway/logout")
+public class SessionTermination extends HttpServlet{
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = Logging.getInstance();
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            session.invalidate();
+            JsonObject msg = new JsonObject();
+            msg.addProperty("msg", "success");
+
+            try {
+                PrintWriter writer = response.getWriter();
+                writer.append(msg.toString());
+                LOGGER.info(msg.toString());
+                writer.close();
+            } catch (IOException e) {
+                LOGGER.severe(e.toString());
+            }
+        }
+    } 
+    
+}
