4faa1430e1dbe54db483862dc6fde91a15600a36
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-03-18 23:02:33 +0000

    Modified the Server util #12 This modification allows for more compacted server calls as print statements are bound to the request itself. Each server call requests information from the session rather than the localstorage which gives it better security #12 #4

--- Application/app/src/main/webapp/utils/Server.js
+++ Application/app/src/main/webapp/utils/Server.js
@@ -2,12 +2,13 @@
 var URL = "http://localhost:8080/app/api/";
 
 function getRequest(url, callback){
+    console.log("URL:", url);
     $.ajax(url,{
         dataType: 'json',
         // dataType: 'text',
         method: 'GET',
-        timeout: 500,
         success: function(data,status,xhr){
+            console.log("response:", data);
             if (callback) callback(data);
         },
         error: function(jqXhr, textStatus, errorMessage){
@@ -17,6 +18,8 @@ function getRequest(url, callback){
 }
 
 function postRequest(url, data, callback){
+    console.log("URL:", url);
+    console.log("request:", data);
     $.ajax(url, {
         dataType: 'json',
         // dataType: 'text',
@@ -24,6 +27,7 @@ function postRequest(url, data, callback){
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify(data),
         success: function (data, status, xhr) {
+            console.log("response:", data);
             if (callback) callback(data);
         },
         error: function (jqXhr, textStatus, errorMessage) {
@@ -35,83 +39,97 @@ function postRequest(url, data, callback){
 
 export function getTransactionsFromUser(callback){
     var url = URL + "prescriptionRequests/getPrescriptionHistory";
-    var ID = getIdentity();
-    this.postRequest(url, ID, function(value){
-        console.log(value);
-        if (callback) callback(value);
-    })
-
+    var self = this;
+    this.verifySession(function(value){
+        var request = {"user" : ""};
+        value.msg = "";
+        request.user = value;
+        self.postRequest(url, request, function(response){
+            if(callback) callback(response);
+        });
+    });
 }
 
 export function getPrescriptionsForUser(callback){
     var url = URL + "prescriptionRequests/getPrescriptionsForUser";
-    var ID = getIdentity();
-    console.log(ID);
-    this.postRequest(url, ID,  function(value){
-        console.log(value);
-        if(callback) callback(value);
+    var self = this;
+    this.verifySession(function(value){
+        var request = {"user" : ""};
+        value.msg = "";
+        request.user = value;
+        self.postRequest(url, request, function(response){
+            if(callback) callback(response);
+        });
     });
 }
 
 export function sendPrescriptionToUser(request, callback){
     var url = URL + "prescriptionRequests/updatePrescriptionOwner";
-    request.user = getIdentity();
-    console.log(request);
-    this.postRequest(url, request, function(value){
-        console.log(value);
-        if(callback) callback(value);
+    var self = this;
+    this.verifySession(function(value){
+        value.msg = "";
+        request.user = value;
+        self.postRequest(url, request, function(response){
+            if(callback) callback(response);
+        });
     });
 }
 
 export function isUser(loginDetails, callback){
-    console.log(loginDetails);
     var url = URL + "userRequestsGateway/userExists";
     this.postRequest(url, loginDetails, function(value){
-        console.log(value);
         if(callback) callback(value);
     });
 }
 
 export function userAuthentication(credentials, callback){
-    var url = URL + "userRequestsGateway/authenticateUser";    
-    console.log(credentials);
+    var url = "/app/userRequestsGateway/authenticateUser";
     this.postRequest(url, credentials, function(value){
-        console.log(value);
         var tempvalue = {"msg" : "success"}
         if(callback) callback(value);
     });
 }
 
-export function getDetailsForUser(request, callback){
-    console.log(request);
+export function getDetailsForUser(callback){
     var url = URL + "userRequestsGateway/getUser"
-    this.postRequest(url, request, function(value){
-        console.log(value);
-        if(callback) callback(value);
-    })
+    var self = this;
+    this.verifySession(function(value){
+        var request = {"user" : "", "identifier" : ""};
+        value.msg = "";
+        request.user = value;
+        request.identifier = value.identifier;
+        self.postRequest(url, request, function(response){
+            if(callback) callback(response);
+        });
+    });
 }
 
 export function createPrescription(request, callback){
-    console.log(request);
-    request.user = getIdentity();
-    console.log(request)
     var url = URL + "prescriptionRequests/createPrescription";
-    this.postRequest(url, request, function(value){
-        console.log(value);
+    var self = this;
+    this.verifySession(function(value){
+        value.msg = "";
+        request.user = value;
+        self.postRequest(url, request, function(response){
+            if(callback) callback(response);
+        });
+    });
+}
+
+export function verifySession(callback){
+    var url = "/app/userRequestsGateway/verifySession";
+    this.getRequest(url, function(value){
         if(callback) callback(value);
+    });
+}
+
+export function endSession(callback){
+    var url = "/app/userRequestsGateway/logout";
+    this.getRequest(url, function(value){
+        if (callback) callback(value);
     })
 }
 
-/*{
-    identity: "dan"
-} */
-function getIdentity(){
-    var user =  { identifier :  localStorage.getItem("ID"), cert : localStorage.getItem("cert")};
-    return user;
-}
-
-
-
 export default {getDetailsForUser, getRequest, postRequest, getTransactionsFromUser, 
     getPrescriptionsForUser, sendPrescriptionToUser, isUser,
-    userAuthentication, createPrescription}
+    userAuthentication, createPrescription, verifySession, endSession}
