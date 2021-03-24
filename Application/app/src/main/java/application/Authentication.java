1e3110946fd2062bff7834cfabd3e0e5a42fc649
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-03-24 21:59:27 +0000

    Added comments and cleaned up code #10

--- Application/app/src/main/java/application/Authentication.java
+++ Application/app/src/main/java/application/Authentication.java
@@ -1,14 +1,10 @@
 package application;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.logging.Logger;
 
+import org.hyperledger.fabric.gateway.Identities;
 import org.hyperledger.fabric.gateway.Wallet;
-import org.hyperledger.fabric.gateway.Wallets;
+import org.hyperledger.fabric.gateway.X509Identity;
 
 import com.google.common.base.CharMatcher;
 import com.google.gson.JsonObject;
@@ -16,15 +12,11 @@ import com.google.gson.JsonParser;
 
 public class Authentication {
     private static final Logger LOGGER = Logging.getInstance();
-    private String walletDIR = "/opt/tomcat/apache-tomcat-8.5.63/webapps/wallets/";
-    // private String walletDIR = "../../wallets";
 
     public JsonObject authenticateUser(JsonObject user) throws Exception {
-        if(userExists(user).equals("success")){
-            String passphrase = readPK(user.get("identifier").getAsString());
-            if(verifyPK(user, passphrase) == 0){
-                user = addPublicKey(user);
-                user = addStatus(user);
+        if(userExists(user)){
+            if(verifyPassphrase(user)){
+                user = addAttr(user);
             } else {
                 return null;
             }
@@ -32,91 +24,69 @@ public class Authentication {
         return user;
     }
 
-    public String userExists(JsonObject user) throws IOException {
-        Path walletpath = Paths.get(walletDIR);
-        Wallet wallet = Wallets.newFileSystemWallet(walletpath);
+    /**
+     * Checks the wallet of the network for a user
+     * 
+     * @param JsonObject user
+     * @return boolean
+     * @throws Exception
+     */
+    public boolean userExists(JsonObject user) throws Exception {
+        Wallet wallet = Connection.getWallet();
         if(wallet.get(user.get("identifier").getAsString()) != null){
             LOGGER.info(user.get("identifier").getAsString() + " found!");
-            return "success";
+            return true;
         } else {
             LOGGER.info(user.get("identifier").getAsString() + " Not found!");
-            return "failure";
+            return false;
         }
     }
 
-    public String readPK(String ppsn){     
-        String pathToCredentials = walletDIR + ppsn + ".id";
-        LOGGER.info("PATH : " + pathToCredentials);
-        try (FileReader reader = new FileReader(pathToCredentials))
-        {
-            Object obj = JsonParser.parseReader(reader);
-            JsonObject usr = (JsonObject) obj;
-            JsonObject credentials = usr.getAsJsonObject("credentials");
-            String privateKey = credentials.get("privateKey").getAsString();
-            return formatPassKey(privateKey);
-
-        } catch (FileNotFoundException e) {
-            LOGGER.severe(e.toString());
-        } catch (IOException e) {
-            LOGGER.severe(e.toString());
+    /**
+     * Retrieves the correct passphrase from the identity 
+     * @param {String} identifier
+     * @return
+     */
+    public String getPassphrase(String identifier){     
+        try {
+            X509Identity user = (X509Identity) Connection.getWallet().get(identifier);
+            String pk =  CharMatcher.breakingWhitespace().removeFrom(Identities.toPemString(user.getPrivateKey()));
+            return pk.substring(217, 225);
+        } catch (Exception e1) {
+            e1.printStackTrace();
         }
         return null;
     }
 
-    public String formatPassKey(String key){
-        String formattedKey = "";
-        System.out.println(key);
-        for(int i = 230; i > 222; i--){
-            formattedKey+= key.charAt(i);
-        }
-        return formattedKey;
-    }
-
-    public int verifyPK(JsonObject user, String passphrase){
+    /**
+     * Verifies the passphrase entered by the user
+     * @param JsonObject user
+     * @return boolean
+     */
+    public boolean verifyPassphrase(JsonObject user){
+        String passphrase = getPassphrase(user.get("identifier").getAsString());
         for(int i = 0; i < user.get("passcode").getAsString().length()-1; i++){
             int indexOfPassPhrase = Character.getNumericValue(user.get("pattern").getAsString().charAt(i));
             char x = passphrase.charAt(indexOfPassPhrase);
             char y = user.get("passcode").getAsString().charAt(i);
             if(Character.compare(x, y) != 0){
                 LOGGER.severe("Incorrect passcode");
-                return -1;
+                return false;
             }
         }
-        return 0;
+        return true;
     }
 
-    public JsonObject addPublicKey(JsonObject user){
-        String ppsn = user.get("identifier").getAsString();
-        String pathToCredentials = walletDIR + ppsn + ".id";
-        try (FileReader reader = new FileReader(pathToCredentials))
-        {
-            Object obj = JsonParser.parseReader(reader);
-            JsonObject usr = (JsonObject) obj;
-            JsonObject credentials = usr.getAsJsonObject("credentials");
-            String cert = credentials.get("certificate").getAsString();
-            cert = CharMatcher.breakingWhitespace().removeFrom(cert);
-            user.addProperty("cert", cert);
-
-        } catch (FileNotFoundException e) {
-            LOGGER.severe(e.toString());
-        } catch (IOException e) {
-            LOGGER.severe(e.toString());
-        }
-        return user;
-    }
-
-    public JsonObject addStatus(JsonObject user){
+    /**
+     * Adds the addtional attributes of the user's certificate and status and returns the user
+     * @param JsonObject user
+     * @return JsonObject user
+     */
+    public JsonObject addAttr(JsonObject user){
         JsonObject allUserDetails = JsonParser.parseString(UserRequests.getUserByIdentifier(user, user.get("identifier").getAsString())).getAsJsonObject();
+        user.addProperty("cert", allUserDetails.get("cert").getAsString());
         user.addProperty("status", allUserDetails.get("status").getAsString());
         return user;
     }
 
-    public boolean verifySession(JsonObject userInfo){
-        JsonObject correctUserDetails = (JsonObject) JsonParser.parseString(UserRequests.getUserByIdentifier(userInfo, userInfo.get("identifier").getAsString()));
-        if(userInfo.get("cert").getAsString().equals(correctUserDetails.get("cert").getAsString()) && userInfo.get("status").getAsString().equals(correctUserDetails.get("status").getAsString())){
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
