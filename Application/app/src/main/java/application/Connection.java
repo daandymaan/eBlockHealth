10ccd50a9db9174ba789aeee4b2644997142763e
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-03-24 21:50:56 +0000

    Added getWallet method to Connection class #10 Cleaned up code and each class now uses the static getWallet method

--- Application/app/src/main/java/application/Connection.java
+++ Application/app/src/main/java/application/Connection.java
@@ -13,20 +13,22 @@ import org.hyperledger.fabric.gateway.Wallet;
 import org.hyperledger.fabric.gateway.Wallets;
 
 public class Connection {
-    // private static String walletDIR = "../wallets";
-    // private static String connectionDIR = "../connection-org1.yaml";
     private static String walletDIR = "/opt/tomcat/apache-tomcat-8.5.63/webapps/wallets/";
     private static String connectionDIR = "/opt/tomcat/apache-tomcat-8.5.63/webapps/app/WEB-INF/classes/connection-org1.yaml";
-
 
     //Needed to run application locally
     static {
 		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
 	}
-    
+    /**
+     * Gets Hyperledger network connection to the blockchain using a user's credentials
+     * 
+     * @param JsonObject user
+     * @return Network network
+     * @throws Exception
+     */
     public static Network connect(JsonObject user) throws Exception{
-        Path walletpath = Paths.get(walletDIR);
-        Wallet wallet = Wallets.newFileSystemWallet(walletpath);
+        Wallet wallet = getWallet();
         Path networkConfigPath = Paths.get(connectionDIR);
         Gateway.Builder builder = Gateway.createBuilder();
         builder.identity(wallet, user.get("identifier").getAsString()).networkConfig(networkConfigPath).discovery(true);
@@ -38,10 +40,28 @@ public class Connection {
 
     }
 
+    /**
+     * Retrieves a specified contract from the network.
+     * @param JsonObject user
+     * @param String contractName
+     * @return Contract contract
+     * @throws Exception
+     */
     public static Contract getContract(JsonObject user, String contractName) throws Exception {
         Network network = connect(user);
         Contract contract = network.getContract(contractName);
         System.out.println("Contract found");
         return contract;
+    }
+
+    /**
+     * Retrieves the wallet with credentials 
+     * @return Wallet wallet
+     * @throws Exception
+     */
+    public static Wallet getWallet() throws Exception{
+        Path walletpath = Paths.get(walletDIR);
+        Wallet wallet = Wallets.newFileSystemWallet(walletpath);
+        return wallet;
     }
 }
