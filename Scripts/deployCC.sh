dc27ad4b8a2ebcb74f79dd3539b9456a5b9b9096
daandymaan <47121977+daandymaan@users.noreply.github.com>
2021-03-28 22:29:26 +0100

    Removed comments from script

--- Scripts/deployCC.sh
+++ Scripts/deployCC.sh
@@ -23,10 +23,6 @@ function println() {
 CHANNEL_NAME="mychannel"
 CC_NAME=$1
 CC_SRC_PATH=$2
-# CC_NAME="infocontract"                #chaincode-java
-# CC_SRC_PATH=/home/dan/Docs/BlockchainPrescribing/Smart-Contract/info-contract
-# CC_NAME="pc"                #chaincode-java
-# CC_SRC_PATH=/home/dan/Docs/BlockchainPrescribing/Smart-Contract/java-contract
 CC_SRC_LANGUAGE=java
 CC_RUNTIME_LANGUAGE=java
 CC_VERSION="1.0"
